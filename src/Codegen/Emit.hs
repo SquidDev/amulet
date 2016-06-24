1d44d589a94d9cb419d3112e4dc5da2b70c51f42
Matheus Magalhães de Alcantara <matheus.de.alcantara@gmail.com>
2016-06-23 21:04:17 -0300

    Changes to support elseif chains

--- src/Codegen/Emit.hs
+++ src/Codegen/Emit.hs
@@ -5,11 +5,19 @@ import Data.List
 
 emitS :: Statement -> String
 emitS (Do xs) = concat ["do ", intercalate "; " $ map emitS xs, " end"]
+emitS (Set n (Function ars dt b)) = concat [ "local function ", emitN n, "(", intercalate ", " ars, ") "
+                                           , intercalate "; " $ map emitS b, " end" ]
 emitS (Set n e) = concat ["local ", emitN n, " = ", emitE e]
 emitS (While c b) = concat ["while ", emitE c, emitS (Do b)]
 emitS (Repeat c b) = concat ["repeat ", intercalate "; " $ map emitS b, " until ", emitE c]
-emitS (If [(e, tru)] els) = concat [ "if ", emitE e, " then ", intercalate "; " $ map emitS tru
-                                   , " else ", intercalate "; " $ map emitS els]
+emitS (If (x:xs) els) = concat [ mkif x, intercalate " " $ map mkeif xs
+                               , mkels els
+                               , " end"]
+    where mkif (x, b)  = concat ["if ", emitE x, " then ", intercalate "; " $ map emitS b]
+          mkeif (x, b) = concat [" elseif ", emitE x, " then ", intercalate "; " $ map emitS b]
+          mkels [] = ""
+          mkels b = concat [" else ", intercalate "; " $ map emitS b]
+
 emitS (Local nms) = concat ["local ", intercalate ", " $ map fst nms, " = ", intercalate ", " $ map (emitE . snd) nms]
 emitS (SApply ap) = emitA ap
 emitS (Return e) = concat ["return ", emitE e]
@@ -32,7 +40,7 @@ emitE (Function ars dt b) = concat [ "function(", intercalate ", " ars, (if dt t
 
 
 emitE (Op l o r) = make r
-  where make (Just x) = concat [emitE l, emitO o, emitE x]
+  where make (Just x) = concat [emitE l, " ", emitO o, " ", emitE x]
         make Nothing = concat [emitO o, emitE l]
 
 emitN :: Name -> String
